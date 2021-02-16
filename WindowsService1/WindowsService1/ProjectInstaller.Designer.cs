c175607b6d3f1e90c008dda502047afb203f2f01
kasimkurt <55134159+kasimkurt@users.noreply.github.com>
2021-02-16 22:32:47 +0300

    ilk yükleme
    ilk yükleme

---
+++ WindowsService1/WindowsService1/ProjectInstaller.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace WindowsService1
+{
+    partial class ProjectInstaller
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.serviceProcessInstaller2 = new System.ServiceProcess.ServiceProcessInstaller();
+            this.serviceInstaller2 = new System.ServiceProcess.ServiceInstaller();
+            // 
+            // serviceProcessInstaller2
+            // 
+            this.serviceProcessInstaller2.Account = System.ServiceProcess.ServiceAccount.LocalSystem;
+            this.serviceProcessInstaller2.Password = null;
+            this.serviceProcessInstaller2.Username = null;
+            // 
+            // serviceInstaller2
+            // 
+            this.serviceInstaller2.Description = "Service1";
+            this.serviceInstaller2.DisplayName = "Service1";
+            this.serviceInstaller2.ServiceName = "Service1";
+            // 
+            // ProjectInstaller
+            // 
+            this.Installers.AddRange(new System.Configuration.Install.Installer[] {
+            this.serviceProcessInstaller2,
+            this.serviceInstaller2});
+
+        }
+
+        #endregion
+
+        private System.ServiceProcess.ServiceProcessInstaller serviceProcessInstaller1;
+        private System.ServiceProcess.ServiceInstaller serviceInstaller1;
+        private System.ServiceProcess.ServiceProcessInstaller serviceProcessInstaller2;
+        private System.ServiceProcess.ServiceInstaller serviceInstaller2;
+    }
+}
---
+++ WindowsService1/WindowsService1/Service1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Xml;
+
+namespace WindowsService1
+{
+    public partial class Service1 : ServiceBase
+    {
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        Timer timer = new Timer();
+
+        protected override void OnStart(string[] args)
+        {
+            //Dosyamıza servis başlangıç zamanını yazdırıyoruz ve timer ayarlarını //yapıyoruz.
+            WriteToFile("Service is started at " + DateTime.Now);
+            timer.Elapsed += new ElapsedEventHandler(OnElapsedTime);
+            timer.Interval = 5000;
+            timer.Enabled = true;
+        }
+
+        protected override void OnStop()
+        {
+        }
+
+        private void OnElapsedTime(Object source, ElapsedEventArgs e) 
+        {
+            WriteToFile("Service is recall at " + DateTime.Now);
+        }
+
+
+        private double Dolar = 0.0;
+        private DataSet dsDovizKur;
+
+        string ht;
+        private void WriteToFile(string Message)
+        {
+
+            dsDovizKur = new DataSet();
+            dsDovizKur.ReadXml(@"http://www.tcmb.gov.tr/kurlar/today.xml");
+            DataRow dr = dsDovizKur.Tables[1].Rows[0];
+            Dolar = Convert.ToDouble(dr[4].ToString().Replace('.', ','));
+
+       
+            try
+            {
+                SqlConnection baglanti = new SqlConnection("Server=.;Database=EfDb;User Id=sa;Password=060792;");
+                SqlCommand komut = new SqlCommand("insert into tbl (t1)values (\'" + Convert.ToString(Dolar).Replace(',', '.') + "\')", baglanti);
+                baglanti.Open();
+                komut.ExecuteNonQuery();
+
+                baglanti.Close();
+            }
+            catch (Exception e)
+            {
+                ht = e.Message;
+
+            }
+
+
+
+
+
+            string path = @"c:\yeni\Log";
+        
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+            string filepath = @"c:\yeni\ss.txt";
+           
+            if (!File.Exists(filepath))
+            {
+            
+                using (StreamWriter sw = File.CreateText(filepath))
+                {
+                    sw.WriteLine(Convert.ToString(Dolar));
+                    sw.WriteLine(ht);
+                }
+
+            }
+            else
+            {
+           
+                using (StreamWriter sw = File.AppendText(filepath))
+                {
+                    sw.WriteLine(Convert.ToString(Dolar));
+                    sw.WriteLine(ht);
+                }
+            }
+        }
+    }
+}
